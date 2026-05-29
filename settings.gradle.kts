ba0e497c52eeca5e2c4d1ef51a77cf8e89bf36bf
harshtomar001 <harshtomar4081@gmail.com>
2026-05-29 13:53:29 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,9 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
-    repositories {
-        google()
         mavenCentral()
     }
 }
+
 rootProject.name = "VertlixUtils"
