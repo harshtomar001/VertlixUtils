07d4a2e2aafe6cfca31e01579bc707068a8a3290
Harsh Tomar <harshtomar4081@gmail.com>
2026-05-29 10:35:08 +0530

    Fix JitPack build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id("maven-publish")
 }
 
 android {
@@ -15,15 +16,5 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-}
-
-dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-
 }
 
